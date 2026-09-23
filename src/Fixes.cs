a6349c24b6ee020db06b842faca5094779177978
Sava Andrei Sebastian <61626661+skuzzis@users.noreply.github.com>
2026-09-23 13:10:38 +0300

    update(fixes): Version

--- src/Fixes.cs
+++ src/Fixes.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.Configuration;
 
 namespace Fixes;
 
-[PluginMetadata(Id = "Fixes", Version = "1.0.16", Name = "Fixes", Author = "Swiftly Development Team", Description = "No description.")]
+[PluginMetadata(Id = "Fixes", Version = "1.0.17", Name = "Fixes", Author = "Swiftly Development Team", Description = "No description.")]
 #pragma warning disable CS9107 // Parameter is captured into the state of the enclosing type and its value is also passed to the base constructor. The value might be captured by the base class as well.
 public partial class Fixes(ISwiftlyCore core) : BasePlugin(core)
 #pragma warning restore CS9107 // Parameter is captured into the state of the enclosing type and its value is also passed to the base constructor. The value might be captured by the base class as well.
